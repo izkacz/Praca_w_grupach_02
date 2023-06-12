29a90f216f7a4432ebdde3bf7ea125b0fdd0f66a
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-06-12 12:35:19 +0200

    fixed do lewej

--- zadanie1.py
+++ zadanie1.py
@@ -1,4 +1,5 @@
 lista_B = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+lista_C = [[1, 7, 909, 1024], [24, 66, 89, 100], [77, 24, 3, 208]]
 
 def znajdz_max(lista):
     max_element = 0
@@ -15,16 +16,14 @@ def do_lewej(lista):
     maxLen = znajdz_max(lista)
     for i in lista:
         additionalList = []
-        for index, j in enumerate(i):
+        for j in i:
             k = str(j)
-            if k < str(maxLen):
-                spacebarAmount = maxLen - len(k)
-                result = k+ " "*spacebarAmount
-                additionalList.append(result)
-                if j == i:
-                    print('\n')
+            result = k.ljust(maxLen)
+            additionalList.append(result)                   
         print(additionalList)
         additionalList.clear()
-
-        
+    print('\n')
+    
 do_lewej(lista_B)
+print('\n')
+do_lewej(lista_C)
