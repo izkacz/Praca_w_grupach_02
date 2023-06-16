96f463947f4f6278f3f8bc5860b897f24bf5179b
Paulina Reus <paularkla@gmail.com>
2023-06-16 20:02:10 +0200

    Dodanie zad 2 z modulu b

--- zadanie2.py
+++ zadanie2.py
@@ -1,3 +1,5 @@
+import math
+
 #zadanieIzy
 def missingRectangleVertice(x1,y1,x2,y2,x3,y3):
     if x1 == x2 or x1==x3:
@@ -19,7 +21,23 @@ def missingRectangleVertice(x1,y1,x2,y2,x3,y3):
 missingRectangleVertice(1,1,2,3,4,2)
 
 #TODO zadaniePauliny
+def missingTrinagleVertice(ax, ay, bx, by):
 
+    # liczmy odleglośc miedzy punktami
+    ab = math.pow(pow(bx - ax, 2) + pow(by - ay, 2), 1 / 2)
+    print(ab)
+
+    angle = math.atan2(by - ay, bx - ax)
+
+    # calculate the coordinates of the third point (the first one)
+    cx_1 = ax + ab * math.cos(angle - (1 * math.pi / 3))
+    cy_1 = ay + ab * math.sin(angle - (1 * math.pi / 3))
+
+    # calculate the coordinates of the third point (the second one)
+    cx_2 = bx + ab * math.cos(angle + (1 * math.pi / 3))
+    cy_2 = by + ab * math.sin(angle + (1 * math.pi / 3))
+
+    return print('Wspołrzedne trójkąta to ', cx_1, " i ", cy_1, ' lub ', cx_2, ' i ', cy_2 )
 
 
 
