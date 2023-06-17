046ee057fa9b639c995863b388a1709f2dc00b3f
Mikołaj Maćkowiak <73481@student.ue.poznan.pl>
2023-06-17 20:39:14 +0200

    added unit tests for rectangle

--- zadanie2.py
+++ zadanie2.py
@@ -50,4 +50,8 @@ def almost_equal(t1, t2, rel_tol=1e-9):
 assert almost_equal(missingTriangleVertice(0, 0, 1, 0)[0], (0.5, math.sqrt(3)/2))
 assert almost_equal(missingTriangleVertice(0, 0, 1, 0)[1], (0.5, -math.sqrt(3)/2))
 
+assert missingRectangleVertice(0, 0, 5, 0, 0, 5) == (5, 5)
+assert missingRectangleVertice(2, 3, 5, 3, 2, 6) == (5, 6)
+
+
 
