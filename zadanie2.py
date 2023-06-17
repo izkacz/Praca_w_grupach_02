95740525f623c6035ae3c50dd79453c50d8ca381
Mikołaj Maćkowiak <73481@student.ue.poznan.pl>
2023-06-17 20:32:10 +0200

    added unit tests and performed minor fixes

--- zadanie2.py
+++ zadanie2.py
@@ -2,26 +2,26 @@ import math
 
 #zadanieIzy
 def missingRectangleVertice(x1,y1,x2,y2,x3,y3):
-    if x1 == x2 or x1==x3:
+    if x1 == x2 or x1 == x3:
         if x1 == x2:
-           x4=x3
+           x4 = x3
         else:
-           x4=x2
+           x4 = x2
     else:
-        x4=x1
-    if y1 == y2 or y1==y3:
+        x4 = x1
+    if y1 == y2 or y1 == y3:
         if y1 == y2:
-           y4=y3
+           y4 = y3
         else:
-           y4=y2
+           y4 = y2
     else:
-        y4=y1
-    return(print(str(x4)+", "+str(y4)))
+        y4 = y1
+    return x4, y4
 
 missingRectangleVertice(1,1,2,3,4,2)
 
 #TODO zadaniePauliny
-def missingTrinagleVertice(ax, ay, bx, by):
+def missingTriangleVertice(ax, ay, bx, by):
 
     # liczmy odleglośc miedzy punktami
     ab = math.pow(pow(bx - ax, 2) + pow(by - ay, 2), 1 / 2)
@@ -30,17 +30,24 @@ def missingTrinagleVertice(ax, ay, bx, by):
     angle = math.atan2(by - ay, bx - ax)
 
     # calculate the coordinates of the third point (the first one)
-    cx_1 = ax + ab * math.cos(angle - (1 * math.pi / 3))
-    cy_1 = ay + ab * math.sin(angle - (1 * math.pi / 3))
+    cx_1 = ax + ab * math.cos(angle + math.pi/3)
+    cy_1 = ay + ab * math.sin(angle + math.pi/3)
 
     # calculate the coordinates of the third point (the second one)
-    cx_2 = bx + ab * math.cos(angle + (1 * math.pi / 3))
-    cy_2 = by + ab * math.sin(angle + (1 * math.pi / 3))
+    cx_2 = ax + ab * math.cos(angle - math.pi/3)
+    cy_2 = ay + ab * math.sin(angle - math.pi/3)
 
-    return print('Wspołrzedne trójkąta to ', cx_1, " i ", cy_1, ' lub ', cx_2, ' i ', cy_2 )
-
+    return [(cx_1, cy_1), (cx_2, cy_2)]
 
 
 
 
 #TODO testyMikolaja
+#almost_equal - rounding numbers due to precission issues
+def almost_equal(t1, t2, rel_tol=1e-9):
+    return all(abs(x-y) <= max(rel_tol * max(abs(x), abs(y)), rel_tol) for x, y in zip(t1, t2))
+
+assert almost_equal(missingTriangleVertice(0, 0, 1, 0)[0], (0.5, math.sqrt(3)/2))
+assert almost_equal(missingTriangleVertice(0, 0, 1, 0)[1], (0.5, -math.sqrt(3)/2))
+
+
